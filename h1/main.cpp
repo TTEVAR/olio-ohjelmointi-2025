34ac074c66031b25f78c9d67a5f06d713c67bb85
TTEVAR <t2teva01@students.oamk.fi>
2025-11-09 22:45:19 +0200

    h1 valmis

--- h1/main.cpp
+++ h1/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
+#include <iostream>
+
 using namespace std;
 
-int main()
-{
-    string etunim;
-    cout << "Anna etunimesi:" << endl;
-    cin >> etunim;
-    cout<<"Tervetuloa "<<etunim <<endl;
+int main() {
+    string name;
+    int age;
+    cout << "mika on nimesi?: ";
+    cin >> name;
+    cout << "kuinka vanha olet?: ";
+    cin >> age;
+    cout << "Terve " << name << "olet siis " << age << " vuotias" << endl;
     system("pause");
     return 0;
 }
