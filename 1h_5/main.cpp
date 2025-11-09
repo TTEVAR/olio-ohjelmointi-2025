ad5c9275da09059092f8ee00363716b29e1e204b
V.A.T <t2teva01@students.oamk.fi>
2025-11-09 22:51:48 +0200

    1h_5 valmis

---
+++ 1h_5/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+/*
+ * Esimerkkiin lisättiin try catch -rakenne vaikkei
+ * sitä ollut tehtävässä pyydetty
+ */
+using namespace std;
+
+int tuplaa(int);
+void luvunTulostus(int);
+
+int main() {
+    int luku;
+    int tulos;
+    cout << "Anna positiivinen kokonaisluku" << endl;
+    cin >> luku;
+    if(luku<0){
+        throw runtime_error("Piti antaa positiivinen luku");
+    }
+    try {
+        tulos=tuplaa(luku);
+        luvunTulostus(tulos);
+    } catch (runtime_error& e) {
+        cout<<e.what()<<endl;
+    }
+
+    system("pause");
+    return 0;
+}
+
+int tuplaa(int a) {
+    return a + a;
+}
+void luvunTulostus(int b){
+    cout<<"Tulos="<<b<<endl;
+}
