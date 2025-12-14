75f9f7eaa65511dc3db6f6ee68f2aa415d2e0572
V.A.T <t2teva01@students.oamk.fi>
2025-12-14 17:30:57 +0200

    h6b valmis

---
+++ h6b/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include "exampleclass.h"
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+    ExampleClass obj;
+    obj.startToWait();
+
+
+    return a.exec();
+}
